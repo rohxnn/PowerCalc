25ae9fa1bc338f2abeaf9c2e72408a94a2922157
Rohan Thomas Reji <rohanreji123@gmail.com>
2023-12-01 18:10:30 +0530

    error

--- src/main/java/com/example/PowerCalc/PowerCalcApplication.java
+++ src/main/java/com/example/PowerCalc/PowerCalcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PowerCalcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PowerCalcApplication.class, args);
+		SpringApplication.run(PowerCalcApplication.class, args
 	}
 
 }
